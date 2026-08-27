2d24a82ac65ac0621d41daf45fd48100e24103b3
闫兆祥 <73688938+yanzhaoxiang9812@users.noreply.github.com>
2026-08-27 14:48:52 +0800

    Update .wrangler/tmp/bundle-xWSmlW/middleware-insertion-facade.js

---
+++ .wrangler/tmp/bundle-xWSmlW/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "D:\\VSC\\Microsoft VS Code\\chatroom-pwa\\src\\index.js";
+				import * as __MIDDLEWARE_0__ from "D:\\VSC\\Microsoft VS Code\\chatroom-pwa\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "D:\\VSC\\Microsoft VS Code\\chatroom-pwa\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
+
+				export * from "D:\\VSC\\Microsoft VS Code\\chatroom-pwa\\src\\index.js";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
